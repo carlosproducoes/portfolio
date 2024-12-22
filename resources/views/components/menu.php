7d2f99b529e49cccb9ef3d27d7200937305e8167
Carlos Gonçalves <carlos.goncalves1193@gmail.com>
2024-12-21 22:57:17 -0300

    Redo the menu

---
+++ resources/views/components/menu.php
@@ -0,0 +1,20 @@
+<link rel="stylesheet" href="css/menu.css">
+
+<header>
+    <a href="#" class="logo">CARLOS GONÇALVES</a>
+
+    <div id="btn-mobile">
+        <span id="hamburger"></span>
+    </div>
+
+    <nav id="menu">
+        <a href="#sobre-mim">Sobre Mim</a>
+        <a href="#meus-servicos">Serviços</a>
+        <a href="#meus-projetos">Meus Projetos</a>
+        <a href="#contato">Contato</a>
+        <a href="https://linkedin.com/in/carlos-gonçalves3" target="_blank"><img src="images/linkedin.svg" alt="Ícone Linkedin" class="icone"></a>
+        <a href="https://github.com/carlosproducoes" target="_blank"><img src="images/github.svg" alt="ìcone Github" class="icone"></a>
+    </nav>
+</header>
+
+<script src="js/menu.js"></script>
